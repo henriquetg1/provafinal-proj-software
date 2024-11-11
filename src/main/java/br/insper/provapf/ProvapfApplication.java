bf4e0c893d40d5d137181e9f60adcdb0cefcd48d
henriquetg1 <henriqueturco1@gmail.com>
2024-11-10 21:05:45 -0300

    init

---
+++ src/main/java/br/insper/provapf/ProvapfApplication.java
@@ -0,0 +1,13 @@
+package br.insper.provapf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvapfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvapfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/insper/provapf/ProvapfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.provapf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvapfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
